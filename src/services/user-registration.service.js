16a19de4120b3885ed102712c0fab72022f8930c
User <java.user.tester@gmail.com>
2023-07-29 12:06:20 +0300

    some data changed

--- src/services/user-registration.service.js
+++ src/services/user-registration.service.js
@@ -1,13 +1,13 @@
 import http from '../http-common';
 class UserDataService {
- getAllUsers() {
- return http.get("/users");
- }
- createUser(user) {
- return http.post("/createUser", user);
- }
- deleteUser(id) {
- return http.delete(`/user/delete/id/${id}`);
- }
+    getAllUsers() {
+        return http.get("/users");
+    }
+    createUser(user) {
+        return http.post("/createUser", user);
+    }
+    deleteUser(id) {
+        return http.delete(`/user/delete/id/${id}`);
+    }
 }
 export default new UserDataService();
