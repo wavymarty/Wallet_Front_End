4761ffcf0752efb69b901b77f57749891740ca4f
martinaleksiev <martin@emailemail.com>
2023-08-29 20:29:06 +0300

    new commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+var root = "root";
+var password = "password";
 export default axios.create({
  baseURL: "http://localhost:8084/user",
  headers: {
  "Content-type": "application/json"
  },
+ auth: {
+
+    username: root,
+
+    password: password
+
+ }
 });
--- src/http-transactions.js
+++ src/http-transactions.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+var root = "root";
+var password = "password";
 export default axios.create({
     baseURL: "http://localhost:8082/blockchain/v1",
     headers: {
         "Content-type": "application/json"
     },
+    auth: {
+
+        username: root,
+    
+        password: password
+    
+     }
 });
